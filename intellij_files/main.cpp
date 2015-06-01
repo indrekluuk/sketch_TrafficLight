9bc66880f5f040d3d597e2dae02e841c5d659c8c
Indrek Luuk <indrek.luuk@gmail.com>
2015-06-02 00:16:15 +0300

    run all controller methods in main

--- intellij_files/main.cpp
+++ intellij_files/main.cpp
@@ -1,15 +1,31 @@
 #include "../CrosswalkController.h"
-#include "../Scheduler.h"
+
+
+
+
+
+void runScheduler(int count) {
+    for (int i=0; i<count; i++) {
+        Scheduler::run();
+    }
+}
+
 
 
 int main() {
 
     CrosswalkController controller(1,2,3,4,5);
-    controller.buttonPressed();
 
-    while(1) {
-        Scheduler::run();
-    }
+    controller.on();
+    runScheduler(1000);
+    controller.buttonPressed();
+    runScheduler(1000);
+    controller.night();
+    runScheduler(1000);
+    controller.off();
+    runScheduler(1000);
 
     return 0;
 }
+
+
